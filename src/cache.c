7fd0d8e3e25db0903530cfe2733e070844208c10
Joe Watkins <krakjoe@php.net>
2021-04-03 10:25:13 +0200

    tidier

--- src/cache.c
+++ src/cache.c
@@ -418,12 +418,8 @@ static zend_function* php_parallel_cache_function_ex(const zend_function *source
                 ZVAL_STR(slot,
                     php_parallel_copy_string_interned(Z_STR_P(literal)));
             }
-
-#if PHP_VERSION_ID >= 70300
-            Z_TYPE_FLAGS_P(slot) = 0;
-#else
-            Z_TYPE_FLAGS_P(slot) &= ~IS_TYPE_REFCOUNTED;
-#endif
+            
+	    Z_TYPE_FLAGS_P(slot) &= ~(IS_TYPE_REFCOUNTED|IS_TYPE_COLLECTABLE);
             literal++;
             slot++;
         }
