1865e66543f32a8fd66c39d8a2572197394fd1df
dixyes <dixyes@gmail.com>
2022-03-11 11:54:37 +0800

    Add arginfo for Channel::__toString

--- src/channel.c
+++ src/channel.c
@@ -271,6 +271,9 @@ PHP_METHOD(Channel, close)
     php_parallel_monitor_unlock(php_parallel_channels.monitor);
 }
 
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(php_parallel_channel___toString_arginfo, 0, 0, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
 PHP_METHOD(Channel, __toString)
 {
     php_parallel_channel_t *channel =
@@ -286,7 +289,7 @@ zend_function_entry php_parallel_channel_methods[] = {
     PHP_ME(Channel, send, php_parallel_channel_send_arginfo, ZEND_ACC_PUBLIC)
     PHP_ME(Channel, recv, php_parallel_channel_recv_arginfo, ZEND_ACC_PUBLIC)
     PHP_ME(Channel, close, php_parallel_channel_close_arginfo, ZEND_ACC_PUBLIC)
-    PHP_ME(Channel, __toString, php_parallel_no_args_arginfo, ZEND_ACC_PUBLIC)
+    PHP_ME(Channel, __toString, php_parallel_channel___toString_arginfo, ZEND_ACC_PUBLIC)
     PHP_FE_END
 };
 
