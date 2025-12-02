d48183448be360ebe404b8560e48da8aae784655
Florian Engelhardt <flo@dotbox.org>
2025-12-02 12:32:22 +0100

    Add a reference to argc and argv to child threads
    This allows `dd-trace-php` to group threads together because for a CLI
    SAPI the script name (derived from argv[0]) is used as the service name
    which profiles get grouped by.

--- src/runtime.c
+++ src/runtime.c
@@ -128,6 +128,8 @@ zend_object* php_parallel_runtime_create(zend_class_entry *type) {
     php_parallel_scheduler_init(runtime);
 
     runtime->parent.server = SG(server_context);
+    runtime->parent.argc = SG(request_info).argc;
+    runtime->parent.argv = SG(request_info).argv;
 
     return &runtime->std;
 }
