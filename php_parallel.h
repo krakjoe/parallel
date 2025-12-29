8bf4ab0c385704fb60c736f10436c329f0521156
Florian Engelhardt <flo@dotbox.org>
2025-12-29 12:22:58 +0100

    fix and bump

--- php_parallel.h
+++ php_parallel.h
@@ -26,7 +26,7 @@
 extern zend_module_entry parallel_module_entry;
 # define phpext_parallel_ptr &parallel_module_entry
 
-# define PHP_PARALLEL_VERSION "1.2.9"
+# define PHP_PARALLEL_VERSION "1.2.10"
 
 # if !defined(ZTS)
 # error Only ZTS build are supported
