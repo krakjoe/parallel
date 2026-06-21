590316236c145d33721c595ef0849880f2631c3b
Florian Engelhardt <florian.engelhardt@datadoghq.com>
2026-06-21 18:58:31 +0200

    version bump

--- php_parallel.h
+++ php_parallel.h
@@ -26,7 +26,7 @@
 extern zend_module_entry parallel_module_entry;
 # define phpext_parallel_ptr &parallel_module_entry
 
-# define PHP_PARALLEL_VERSION "1.2.14"
+# define PHP_PARALLEL_VERSION "1.2.15"
 
 # if !defined(ZTS)
 # error Only ZTS build are supported
